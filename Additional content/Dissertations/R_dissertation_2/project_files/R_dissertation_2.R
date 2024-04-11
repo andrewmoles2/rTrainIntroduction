ed1e805c1a51b30821c7381d824d3e9884ec5732
Andrew Moles <andrewmoles2@gmail.com>
2024-04-11 14:45:42 +0100

    update dissertation files

---
+++ Additional content/Dissertations/R_dissertation_2/project_files/R_dissertation_2.R
@@ -0,0 +1,34 @@
+# setup #
+# load libraries ----
+
+
+# load in dataset ----
+
+
+# clean your data #
+# fix height and weight columns ----
+
+
+# make new columns in the dataset with your calculations for each person (BMI, WHR, Blood pressure) ----
+
+
+
+# rename NA values in occupation as "unemployed" ----
+
+
+
+# add bmi and whr ranges ----
+
+
+
+# join data using lsoa information ----
+
+
+
+# tidy up columns ----
+
+
+
+# write/save data ----
+
+
